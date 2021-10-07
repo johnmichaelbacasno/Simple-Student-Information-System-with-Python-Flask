73d571ed479ef531793bf7bc1b177a8ff5910de5
John Michael Bacasno <79495944+johnmichaelbacasno@users.noreply.github.com>
2021-10-07 19:40:53 +0800

    Added for dashboard blueprint

---
+++ app/views/dashboard.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+
+from .. manage import *
+
+dashboard = Blueprint('dashboard', __name__)
+
+@dashboard.route('/')
+@dashboard.route('/home')
+def dashboard_content():
+    return render_template('dashboard/dashboard.html',
+                            count_students=count_students(),
+                            count_courses=count_courses(),
+                            count_colleges=count_colleges())
