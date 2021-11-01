74ce20c2c00e5953dfb943c5d3a32020388f1f3a
John Michael Bacasno <johnmichael.bacasno@g.msuiit.edu.ph>
2021-11-01 12:04:19 +0800

    Use os module to get environment variables

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
+from os import environ
+
 class Config(object):
     DEBUG = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SECRET_KEY = '1234'
+    SECRET_KEY = environ.get('SECRET_KEY')
 
-    MYSQL_DATABASE_HOST = 'localhost'
-    MYSQL_DATABASE_USER = 'root'
-    MYSQL_DATABASE_PASSWORD = 'localhost1234'
-    MYSQL_DATABASE_DB = 'ssis'
+    MYSQL_DATABASE_HOST = environ.get('MYSQL_DATABASE_HOST')
+    MYSQL_DATABASE_USER = environ.get('MYSQL_DATABASE_USER')
+    MYSQL_DATABASE_PASSWORD = environ.get('MYSQL_DATABASE_PASSWORD')
+    MYSQL_DATABASE_DB = environ.get('MYSQL_DATABASE_DB')
 
     BOOTSTRAP_SERVE_LOCAL = True
     BOOTSTRAP_BOOTSWATCH_THEME = None
