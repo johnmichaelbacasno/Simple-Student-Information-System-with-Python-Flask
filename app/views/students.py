f54adbfa9a01dd2b9764f430873b0be058cadaef
John Michael Bacasno <johnmichael.bacasno@g.msuiit.edu.ph>
2021-11-01 17:48:11 +0800

    Add Cloudinary features

--- app/views/students.py
+++ app/views/students.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, flash
 import pymysql
 
-from extensions import db
+from extensions import db, cloudinary
+from cloudinary.uploader import upload
+from cloudinary.utils import cloudinary_url
 
 from .. forms import StudentAddForm, StudentEditForm
 from .. manage import available_students, course_options
@@ -19,9 +21,11 @@ def add_student():
     try:
         if form.validate_on_submit():
             query = """
-                INSERT INTO `Student`(`id`, `first_name`, `middle_name`, `last_name`, `course`, `year`, `birth_date`, `birth_place`, `sex`, `gender`, `civil_status`, `citizenship`, `address`, `contact_number`)
-                VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                INSERT INTO `Student`(`id`, `first_name`, `middle_name`, `last_name`, `course`, `year`, `birth_date`, `birth_place`, `sex`, `gender`, `civil_status`, `citizenship`, `address`, `contact_number`, `image_url`)
+                VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                 """
+            
+            image_url, _ = cloudinary_url(f'{form.id.data}.jpg')
 
             data = (
                 form.id.data,
@@ -37,8 +41,12 @@ def add_student():
                 form.civil_status.data,
                 form.citizenship.data,
                 form.address.data,
-                form.contact_number.data
+                form.contact_number.data,
+                image_url
                 )
+            
+            if form.image_file.data:
+                upload(form.image_file.data.read(), public_id=f"ssis/student_images/{form.id.data}")
 
             conn = db.connect()
             cursor = conn.cursor()
@@ -60,20 +68,27 @@ def edit_student(id):
     try:
         conn = db.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM `Student` WHERE `id` = %s", (id,))
+        cursor.execute("SELECT `id`, `first_name`, `middle_name`, `last_name`, `course`, `year`, `birth_date`, `birth_place`, `sex`, `gender`, `civil_status`, `citizenship`, `address`, `contact_number` FROM `Student` WHERE `id` = %s", (id,))
         row = cursor.fetchone()
         cursor.close()
         conn.close()
-
+        
+        conn = db.connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT `image_url` FROM `Student` WHERE `id` = %s", (id,))
+        image_url = cursor.fetchone()[0]
+        cursor.close()
+        conn.close()
+        
         form = StudentEditForm(data=row)
         form.course.choices += course_options()
-
+        
         if form.validate_on_submit():
             query = """
                 UPDATE `Student` SET `first_name` = %s, `middle_name` = %s, `last_name` = %s, `course` = %s, `year` = %s, `birth_date` = %s, `birth_place` = %s, `sex` = %s, `gender` = %s, `civil_status` = %s, `citizenship` = %s, `address` = %s, `contact_number` = %s
                 WHERE `id` = %s
                 """
-			
+            
             data = (
                 form.first_name.data,
                 form.middle_name.data,
@@ -88,8 +103,11 @@ def edit_student(id):
                 form.citizenship.data,
                 form.address.data,
                 form.contact_number.data,
-                form.id.data
+                form.id.data,
                 )
+            
+            if form.image_file.data:
+                upload(form.image_file.data.read(), public_id=f"ssis/student_images/{form.id.data}")
 			
             conn = db.connect()
             cursor = conn.cursor()
@@ -100,7 +118,7 @@ def edit_student(id):
 			
             flash('Student updated successfully!', 'success')
 
-        return render_template('students/edit_student.html', form=form)
+        return render_template('students/edit_student.html', form=form, image_url=image_url)
     except Exception as exception:
         return str(exception)
 
