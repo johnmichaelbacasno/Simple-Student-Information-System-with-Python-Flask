24f8fca53c33d678dcb3ab59cdabac98332c94eb
John Michael Bacasno <johnmichael.bacasno@g.msuiit.edu.ph>
2021-11-01 17:35:06 +0800

    Add Cloudinary configurations

--- extensions.py
+++ extensions.py
@@ -1,7 +1,16 @@
+from os import environ
 from flaskext.mysql import MySQL
 from flask_bootstrap import Bootstrap
 from flask_wtf import CSRFProtect
+import cloudinary
 
 db = MySQL()
 bootstrap = Bootstrap()
 csrf = CSRFProtect()
+
+cloudinary.config(
+    cloud_name = environ.get('CLOUDINARY_CLOUD_NAME'), 
+    api_key = environ.get('CLOUDINARY_API_KEY'), 
+    api_secret = environ.get('CLOUDINARY_API_SECRET'),
+    secure = environ.get('CLOUDINARY_API_SECRET')
+)
